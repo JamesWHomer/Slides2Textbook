0997536208e7761e80725b042d9c7b6b7fe18e1a
James Homer <jameswatsonhomer@gmail.com>
2025-09-15 22:34:12 +1000

    simple main.py flow

---
+++ main.py
@@ -0,0 +1,19 @@
+# The main orchestrator.
+import pdf_decoder
+import llm_tools
+import md_saver
+import mdToPdf
+
+def main():
+    print("Starting SlidesToTextbook")
+    md = pdf_decoder.to_md("06-LogicalDesignTheory-2025-v1.pdf")
+    print("Converted slides to markdown file")
+    chapter = llm_tools.get_chapter(md)
+    print("Converted slides to longform textbook")
+    md_saver.save_md(chapter, "output", "textbook")
+    print("Saved markdown file of textbook")
+    mdToPdf.mdToPdf(chapter, "output", "textbook")
+    # print(chapter)
+
+if __name__ == "__main__":
+    main()
