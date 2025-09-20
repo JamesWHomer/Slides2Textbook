0bfd0bc1c79a7d361aa492a190fc4e34b224687e
James Homer <jameswatsonhomer@gmail.com>
2025-09-20 20:45:45 +1000

    finish basic cli

--- slides2textbook/main.py
+++ slides2textbook/main.py
@@ -1,4 +1,5 @@
 # The main orchestrator.
+from dataclasses import make_dataclass
 from slides2textbook import pdf_decoder
 from slides2textbook import llm_tools
 from slides2textbook import md_saver
@@ -31,25 +32,35 @@ def main(argv: list[str] | None = None) -> None:
     
 
 
-def run_pipeline(pdf: Path, txt: Path | None, out_dir: Path, name: str, save_md: bool, make_pdf: bool) -> None:
+def run_pipeline(pdf: Path | None, txt: Path | None, out_dir: Path, name: str, save_md: bool, make_pdf: bool) -> None:
+    out_dir.mkdir(parents=True, exist_ok=True)
+
     print("Starting SlidesToTextbook, now loading context.")
-    md = pdf_decoder.to_md("input\ISYS7.pdf")
-    trans = text_loader.load_txt("input\ISYS7.txt")
+    # TODO: modify to_md and load_txt to accept Path
+    md = pdf_decoder.to_md(str(pdf)) if pdf else ""
+    trans = text_loader.load_txt(str(txt)) if txt else ""
     context = llm_tools.context_creator(markdown_file=md, transcript=trans)
+
     print("Loaded context, beginning to generate chapter.")
     chapter = llm_tools.to_chapter(context)
     print("Converted slides to longform textbook")
-    md_saver.save_md(chapter, "output", "ISYS7")
-    md_to_pdf.mdToPdf(chapter, "output", "ISYS7")
-    print("Saved markdown file and pdf.")
+    
+    if save_md:
+        md_saver.save_md(chapter, str(out_dir), name)
+        print("Saved markdown to %s/%s", out_dir, name)
+    if (make_pdf):
+        md_to_pdf.mdToPdf(chapter, str(out_dir), name)
+        print("Saved PDF to %s/%s", out_dir, name)
+    if not save_md and not make_pdf:
+        print("Nothing saved as both --no-md and --no-pdf flags were set. ")
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        prog='Slides2Textbook',
-        description="Allows you to convert pdf's and other context into high quality textbooks.",
+        prog='Slide2Textbook',
+        description="Slide2Textbook allows you to convert pdf's and other context into high quality textbooks.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("--pdf", required=True, type=existing_file, help="Path to input slides PDF")
+    parser.add_argument("--pdf", type=existing_file, help="Path to input slides PDF")
     parser.add_argument("--txt", type=existing_file, help="Path to input txt context")
     parser.add_argument("-o", "--out-dir", type=Path, default=Path("output"), help="Directory to place outputs")
     parser.add_argument("-n", "--name", help="Basename for outputs (defaults to PDF filename)")
