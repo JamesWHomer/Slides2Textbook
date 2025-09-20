6d8d3323e0abe94125f26168dae1d76745ed7494
James Homer <jameswatsonhomer@gmail.com>
2025-09-20 17:39:21 +1000

    previous edits

--- slides2textbook/main.py
+++ slides2textbook/main.py
@@ -7,16 +7,14 @@ from slides2textbook import text_loader
 
 def main():
     print("Starting SlidesToTextbook, now loading context.")
-    md = pdf_decoder.to_md("input\L - week 7 - intro to Turing Machines.pdf")
-    trans = text_loader.load_txt("input\Week 07 - Models of -transcript.txt")
-    # context = llm_tools.context_creator(markdown_file=md, transcript=trans)
-    context = llm_tools.context_creator(transcript=trans)
-    # print(context)
+    md = pdf_decoder.to_md("input\ISYS7.pdf")
+    trans = text_loader.load_txt("input\ISYS7.txt")
+    context = llm_tools.context_creator(markdown_file=md, transcript=trans)
     print("Loaded context, beginning to generate chapter.")
     chapter = llm_tools.to_chapter(context)
     print("Converted slides to longform textbook")
-    md_saver.save_md(chapter, "output", "2922_lecture7_trans")
-    md_to_pdf.mdToPdf(chapter, "output", "2922_lecture7_trans")
+    md_saver.save_md(chapter, "output", "ISYS7")
+    md_to_pdf.mdToPdf(chapter, "output", "ISYS7")
     print("Saved markdown file and pdf.")
 
 if __name__ == "__main__":
