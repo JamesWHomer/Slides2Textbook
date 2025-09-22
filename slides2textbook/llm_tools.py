1114e23ebc49937bed51d6afa4b18baa08e92cb5
James Homer <jameswatsonhomer@gmail.com>
2025-09-22 23:25:01 +1000

    implemented system prompt builder

--- slides2textbook/llm_tools.py
+++ slides2textbook/llm_tools.py
@@ -2,6 +2,7 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from slides2textbook import prompt_builder as pb
 
 load_dotenv()
 
@@ -10,19 +11,7 @@ if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY environment variable not set.")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-SYSTEM_PROMPT = ("You are a textbook chapter creator. Given a possibly badly structured markdown file likely created from pdf's/slides "
-    "and missing information such as images, you will attempt to the best of your ability to create a high quality textbook chapter "
-    "of the slides/notes in the markdown file provided, aiming to best educate the reader. "
-    "Base the textbook heavily off of the slides, do not miss any information or go too off topic from the textbook unless necessary. "
-    "Respond in markdown format and assume that your response will be included in the chapter, "
-    "so do not respond with anything but the textbook. "
-    "Do not include unecessary artifacts from the previous format such as page numbers or repeated information such as headers. "
-    "Do not include extra features such as exercises unless they are explicitely shown in the slides. Remember your tasks is to create a chapter based on slides, not a chapter itself. "
-    "Note that a transcript will also possibly be attached from any lecture using that slide. Use the information to augment and determine what other information to include. "
-    "Note that you will not able to see any images or create any images. "
-    "The textbook chapter should be considered standalone. "
-    "Do not start with a preamble, go into the textbook chapter immediately. "
-    )
+SYSTEM_PROMPT = pb.get_system([0,1,2,3,4,5,6,7,8]) # TODO: This is kinda stupid for now.
 
 def context_creator(**contexts):
     context = ""
