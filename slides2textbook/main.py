4e3b341560a56ce13d305f8a31de9c8f918665d5
James Homer <jameswatsonhomer@gmail.com>
2026-01-31 23:06:19 +0100

    complete textbook pipeline

--- slides2textbook/main.py
+++ slides2textbook/main.py
@@ -20,7 +20,7 @@ def main(argv: list[str] | None = None) -> None:
 
     try:
         run_pipeline(
-            paths=args.context_paths,
+            path=args.context_path,
             out_dir=args.out_dir,
             name=name,
             save_md = args.save_md,
@@ -33,30 +33,39 @@ def main(argv: list[str] | None = None) -> None:
     
 
 
-def run_pipeline(paths: list[Path], out_dir: Path, name: str, save_md: bool, make_pdf: bool, model: str) -> None:
+def run_pipeline(path: Path, out_dir: Path, name: str, save_md: bool, make_pdf: bool, model: str) -> None:
     out_dir.mkdir(parents=True, exist_ok=True)
 
     logger.info("Starting SlidesToTextbook, now loading context.")
 
-    context = context_loader.load_context(paths)
+    context: list[str] = context_loader.load_main_directory(path)
 
-    
-    logger.info("Loaded context, beginning to generate chapter.")
+    logger.info(f"Loaded context of character length {len(context)}, beginning to generate textbook.")
 
     input_tokens = output_tokens = 0
-
     SYSTEM_PROMPT = pb.build_system_prompt()
-    inp, out, chapter = llm_tools.generate(SYSTEM_PROMPT, context, model=model)
-    input_tokens += inp
-    output_tokens += out
+    textbook: list[str] = []
+
+    for idx, chapter_context in enumerate(context):
+        if idx > 0:
+            chapter_context = "Previous chapter: \n" + textbook[-1] + "\n\n" + "Current chapter input context: \n\n" + chapter_context
+        inp, out, chapter = llm_tools.generate(SYSTEM_PROMPT, chapter_context, model=model)
+        textbook.append(chapter)
+        input_tokens += inp
+        output_tokens += out
+        logger.info("Finished generating chapter: " + chapter[:100])
 
     logger.info(f"Converted slides to longform textbook. Total Input Tokens: {input_tokens}, Total Output Tokens: {output_tokens}")
+
+    textbook_str = ""
+    for chapter in textbook:
+        textbook_str += chapter
     
     if save_md:
-        md_saver.save_md(chapter, str(out_dir), name)
+        md_saver.save_md(textbook_str, str(out_dir), name)
         logger.info(f"Saved markdown to {out_dir}/{name}")
-    if (make_pdf):
-        md_to_pdf.mdToPdf(chapter, str(out_dir), name)
+    if make_pdf:
+        md_to_pdf.mdToPdf(textbook_str, str(out_dir), name)
         logger.info(f"Saved PDF to {out_dir}/{name}")
     if not save_md and not make_pdf:
         logger.warning("Nothing saved as both --no-md and --no-pdf flags were set. ")
