466d2cba055b4ba531615894dc073c0e27b1a874
James Homer <jameswatsonhomer@gmail.com>
2026-02-06 16:42:56 +0100

    Table of contents option and pandoc

--- slides2textbook/md_helper.py
+++ slides2textbook/md_helper.py
@@ -3,7 +3,6 @@ Module for saving and converting Markdown data.
 """
 
 from pathlib import Path
-from markdown_pdf import MarkdownPdf, Section
 import pypandoc
 
 def save_md(md: str, out_dir: Path, name: str) -> None:
@@ -13,30 +12,47 @@ def save_md(md: str, out_dir: Path, name: str) -> None:
     out_dir.mkdir(parents=True, exist_ok=True)
     (out_dir / f"{name}.md").write_text(md, encoding="utf-8")
 
-def md_to_pdf(md: str, out_dir: Path, name: str) -> None:
+def md_to_pdf(md: str, out_dir: Path, name: str, toc: bool = False) -> None:
     """
-    Converts md string to a PDF using markdown-pdf and saves it to out_dir/name.pdf
+    Converts md string to a PDF using pandoc and saves it to out_dir/name.pdf
     """
     out_dir.mkdir(parents=True, exist_ok=True)
-    pdf_path = out_dir / f"{name}.pdf"
+    out_path = out_dir / f"{name}.pdf"
 
-    pdf = MarkdownPdf(toc_level=2, optimize=True)
-    pdf.add_section(Section(md))
-    pdf.save(str(pdf_path))
+    extra_args = [
+        "--variable=geometry:margin=1in",
+        "--variable=fontsize=11pt",
+        "--variable=linestretch=1.15",
+        "--mathml",
+    ]
+    if toc:
+        extra_args.append("--toc")
 
-def md_to_epub(md: str, out_dir: Path, name: str) -> None:
+    pypandoc.convert_text(
+        md,
+        "pdf",
+        format="markdown+tex_math_single_backslash+tex_math_dollars",
+        outputfile=str(out_path),
+        extra_args=extra_args,
+    )
+
+def md_to_epub(md: str, out_dir: Path, name: str, toc: bool = False) -> None:
     """
     Converts md string to an EPUB using pandoc and saves it to out_dir/name.epub
     """
     out_dir.mkdir(parents=True, exist_ok=True)
-    epub_path = out_dir / f"{name}.epub"
+    out_path = out_dir / f"{name}.epub"
+
+    extra_args = [
+        "--mathml",
+    ]
+    if toc:
+        extra_args.append("--toc")
 
     pypandoc.convert_text(
         md,
         "epub3",
         format="markdown+tex_math_single_backslash+tex_math_dollars",
-        outputfile=str(epub_path),
-        extra_args=[
-            "--mathml",
-        ],
+        outputfile=str(out_path),
+        extra_args=extra_args,
     )
