c6c18c2ddbfe334fe10a6d7877de94b0bb218d9b
James Homer <jameswatsonhomer@gmail.com>
2026-02-12 00:20:10 +0100

    clean up imports

--- slides2textbook/llm_tools.py
+++ slides2textbook/llm_tools.py
@@ -1,12 +1,12 @@
-# Handles OpenAI api calls.
 from dataclasses import dataclass
+from functools import lru_cache
 import os
-from typing import Optional, Type
+
+from enum import Enum
 
 from dotenv import load_dotenv
 from openai import OpenAI
 from openai.types.responses import Response, ResponseUsage
-from pydantic import BaseModel
 
 load_dotenv()
 
@@ -66,6 +66,31 @@ def generate_openai(developer: str, user: str, model: str = "gpt-5.2", effort: s
         instructions=developer,
         input=user,
     )
+
+def generate(developer: str, user: str, model_str: str = "openai/gpt-5.2", effort: str = None) -> Response:
+    """Generate and return the output of a call to the various API's. No streaming supported.
+
+    Args:
+        developer: Developer message also known as System Prompt. Instructions that the LLM follows closely.
+        user: User prompt which in our usecase includes context that the LLM can use to generate text. Used less for instruction following than developer.
+        model: The model string used by the API for text generation. Will determine model provider from model of format '<provider>/<model>'.
+        effort: The reasoning/thinking effort that the model uses. For example none/minimal/low/medium/high. Higher effort -> Higher latency.
+
+    Returns:
+        The complete, completed output of the API call. 
+    """
+    model = determine_model(model_str)
+    provider = determine_provider(model_str)
+    match provider:
+        case ModelProvider.OPENAI:
+            return generate_openai(developer, user, model, effort)
+        case ModelProvider.GOOGLE:
+            raise NotImplementedError("Google provider is not yet supported.")
+        case ModelProvider.ANTHROPIC:
+            raise NotImplementedError("Anthropic provider is not yet supported.")
+        case _:
+            raise ValueError(f"Unsupported model provider for model_str={model_str!r}: {provider!r}. Currently only 'openai', 'google' and 'anthropic' are supported.")
+        
 
 @dataclass
 class TokenCount:
